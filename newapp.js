b4d8b2c2b3f7a12db85ae7110203d15085f2fdaf
puneet2121 <bedipuneet21@gmail.com>
2022-10-29 16:11:31 -0600

    first commit

---
+++ newapp.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const request = require('request');
+const port = 3000;
+
+app.use(bodyParser.urlencoded({extended:true}))
+app.get('/',(req,res) => {
+  res.sendFile(__dirname + '/signup.html')
+})
+app.post('/',(req,res) => {
+  console.log(req.body)
+  email = req.body.email;
+  password = req.body.password;
+})
+
+app.listen(port,() => {
+  console.log('the server is running')
+})
